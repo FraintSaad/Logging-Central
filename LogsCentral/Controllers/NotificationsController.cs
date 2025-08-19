0e0485fcc7a0a0353a0c65b3b3063c4307a90176
Islam <Superuser@DESKTOP-I93DVCU>
2025-08-19 12:41:18 +0300

    commit with test email in NotificationController

--- LogsCentral/Controllers/NotificationsController.cs
+++ LogsCentral/Controllers/NotificationsController.cs
@@ -1,8 +1,11 @@
 ﻿using Data.Context;
 using Data.Entities;
+using Data.Models;
 using LogsCentral.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
+using System.Net.Mail;
 
 namespace LogsCentral.Controllers
 {
@@ -15,8 +18,16 @@ namespace LogsCentral.Controllers
             _dbContext = dbContext;
         }
         [HttpGet("notifications")]
-        public async Task<IActionResult> IndexAsync()
+        public async Task<IActionResult> Index()
         {
+            _dbContext.Logs.Add(new LogEntity
+            {
+                Timestamp = DateTime.Now,
+                Level = "Error",
+                Message = "Test error log"
+            });
+            _dbContext.SaveChanges();
+
             var configs = await _dbContext.Notifications.ToListAsync();
             var models = configs.Select(c => new NotificationsViewModel
             {
@@ -27,8 +38,8 @@ namespace LogsCentral.Controllers
                 LogLevels = c.LogLevels,
                 Email = c.Email
             }).ToList();
-           
 
+            TestEmail();
             return View(models);
         }
         [HttpPost("add")]
@@ -60,6 +71,29 @@ namespace LogsCentral.Controllers
             }
 
             return RedirectToAction("Index");
+        }
+
+        public IActionResult TestEmail()
+        {
+            var from = "gtbartbrsynts7@gmail.com";  
+            var to = "islamqwertyu@mail.ru"; 
+            var password = "ocnm sjcb jfho jlly";
+
+            using (var client = new SmtpClient("smtp.gmail.com", 587))
+            {
+                client.Credentials = new NetworkCredential(from, password);
+                client.EnableSsl = true;
+
+                var mail = new MailMessage(from, to)
+                {
+                    Subject = "Тестовое письмо",
+                    Body = "Привет! Это тест отправки логов :)"
+                };
+
+                client.Send(mail);
+            }
+
+            return Ok("Письмо отправлено!");
         }
     }
 }
